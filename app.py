b3b407b2aa3eab449d0bed37c7865167003146de
PaulCari <pcaril@unsa.edu.pe>
2026-04-08 12:50:10 -0500

    Añadimos app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, abort
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['UPLOAD_PATH'] = 'uploads'
 
 @app.route('/')
 def index():
@@ -10,5 +16,9 @@ def index():
 def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
+        filename = secure_filename(uploaded_file.filename)
+        extension = os.path.splitext(filename)[1]
+        if extension not in app.config['UPLOAD_EXTENSIONS']:
+            abort(400)
+        uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
     return redirect(url_for('index'))
