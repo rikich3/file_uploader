42b531c60caa9e24e6a68ce704b150a9f3d73b64
Jhonatan David Arias Quispe <jariasq@unsa.edu.pe>
2026-04-08 17:53:43 -0500

    Agregar funcionalidad para mostrar archivos subidos en la página principal

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import imghdr                      # valida contenido real del archivo
 import os
-from flask import Flask, render_template, request, redirect, url_for, abort
+from flask import Flask, render_template, request, redirect, url_for, abort, send_from_directory
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -20,7 +20,8 @@ def validate_image(stream):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    files = os.listdir(app.config['UPLOAD_PATH']) if os.path.exists(app.config['UPLOAD_PATH']) else []
+    return render_template('index.html', files=files)
 
 @app.route('/', methods=['POST'])
 def upload_file():
@@ -37,6 +38,10 @@ def upload_file():
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
     return redirect(url_for('index'))
 
+
+@app.route('/uploads/<filename>')
+def upload(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 # ejecutar servidor
 if __name__ == '__main__':
